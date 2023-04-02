35b4ea4b9a129494855faa0f48a55f80e94fb384
Seraphine Goh <seraphinegoh@gmail.com>
2023-04-02 16:04:05 -0700

    Update card format

--- components/_card.js
+++ components/_card.js
@@ -9,9 +9,7 @@ export default function Card({ href, label, src }) {
     return (
         <>
             <Link href={href} className={styles.card}>
-                <h2 className={inter.className}>
-                {label} <span>-&gt;</span>
-                </h2>
+                <h2 className={inter.className}>{label} <span>-&gt;</span></h2>
                 <Image src={src} alt={label} width={200} height={150} />
             </Link>
         </>
