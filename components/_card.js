602f84fbf516f1bbade61de8dd1a5bd59a4ea63f
Seraphine Goh <seraphinegoh@gmail.com>
2023-04-02 15:22:44 -0700

    Create initial recipe card grid

---
+++ components/_card.js
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import Link from 'next/link';
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Card({ href, label, src }) {
+    return (
+        <>
+            <Link href={href} className={styles.card}>
+                <h2 className={inter.className}>
+                {label} <span>-&gt;</span>
+                </h2>
+                <Image src={src} alt={label} width={200} height={150} />
+            </Link>
+        </>
+    )
+}
