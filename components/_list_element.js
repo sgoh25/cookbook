9ec3737406530109213c60e467ce4eaab915db86
Seraphine Goh <seraphinegoh@gmail.com>
2023-04-03 10:04:25 -0700

    Add strikethrough when checkbox is checked

--- components/_list_element.js
+++ components/_list_element.js
@@ -2,7 +2,7 @@ export default function ListElement({ label }) {
     return (
         <div>
             <input type="checkbox" id="checkbox" />
-            <label htmlFor='checkbox'>{label}</label>
+            <label htmlFor="checkbox" className="strikethrough">{label}</label>
         </div>
     )
 }
