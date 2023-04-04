7eeaee1166bd51c7d85c4794ce8d0121c7ad0366
Seraphine Goh <seraphinegoh@gmail.com>
2023-04-03 21:05:48 -0700

    Update to single recipe json file

--- txt_to_json.py
+++ txt_to_json.py
@@ -15,8 +15,9 @@ def format_str(key, s, index):
     return index, s
 
 
-def convert_to_json(in_file, output):
-    data_dict = {}
+def convert_to_json(in_file, data_dict):
+    data_dict[in_file.stem] = {}
+    in_data = data_dict[in_file.stem]
     with open(in_file) as f:
         is_title = True
         key = ""
@@ -24,7 +25,7 @@ def convert_to_json(in_file, output):
         for line in f:
             line_str = line.strip("\n").rstrip()
             if is_title:
-                data_dict["Title"] = line_str
+                in_data["Title"] = line_str
                 is_title = False
             elif line_str == "Statistics:":
                 key = "Statistics"
@@ -36,46 +37,52 @@ def convert_to_json(in_file, output):
                 if not key or line == "\n":
                     continue
                 index, format_s = format_str(key, line_str, index)
-                if key not in data_dict:
-                    data_dict[key] = [format_s]
+                if key not in in_data:
+                    in_data[key] = [format_s]
                 else:
-                    data_dict[key].append(format_s)
-
-    if output.is_dir():
-        out_file = Path(output) / f"{Path(in_file).stem}.json"
-    elif output.is_file():
-        out_file = Path(output)
-    with open(out_file, "w") as out:
-        json.dump(data_dict, out, indent=4)
+                    in_data[key].append(format_s)
+    return data_dict
 
 
-def parse_path(path, output):
-    if path.is_dir():
-        for p in path.iterdir():
-            parse_path(p, output)
-    elif path.is_file() and path.suffix == ".txt":
-        convert_to_json(path, output)
+def parse_path(in_path, data_dict):
+    if in_path.is_dir():
+        for p in in_path.iterdir():
+            data_dict.update(parse_path(p, data_dict))
+    elif in_path.is_file() and in_path.suffix == ".txt":
+        data_dict.update(convert_to_json(in_path, data_dict))
+    return data_dict
 
 
 def main():
     parser = argparse.ArgumentParser(description="Convert txt file to json")
     parser.add_argument(
-        "path",
-        metavar="PATH",
+        "--input",
+        "-i",
+        metavar="INPUT",
         type=Path,
+        default=Path("public/recipe_txt"),
         help="Path to txt file or directory",
     )
     parser.add_argument(
         "--output",
         "-o",
-        metavar="PATH",
+        metavar="OUTPUT",
         type=Path,
-        default="public/recipe_json",
+        default=Path("public/recipes.json"),
         help="Path for output json file",
     )
     args = parser.parse_args()
 
-    parse_path(args.path, args.output)
+    new_data = parse_path(args.input, {})
+
+    with open(args.output, "r") as out:
+        try:
+            out_data = json.load(out)
+            out_data.update(new_data)
+        except json.decoder.JSONDecodeError:
+            out_data = new_data
+    with open(args.output, "w") as out:
+        json.dump(dict(sorted(new_data.items())), out, indent=4)
 
 
 if __name__ == "__main__":
