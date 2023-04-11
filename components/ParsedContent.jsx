521d09c7775e5a3f09168b360d3377e081e1a28c
Seraphine Goh <seraphinegoh@gmail.com>
2023-04-11 12:01:07 -0700

    Read data from firebase for recipe page

--- components/ParsedContent.jsx
+++ components/ParsedContent.jsx
@@ -2,20 +2,20 @@ import ListElement from '@/components/ListElement.jsx'
 
 export default function ParsedContent(jsonData) {
     let title, stats, ingr, dirs;
-    title = jsonData.Title
+    title = jsonData.title
     stats = (
         <>
-            {jsonData.Statistics.map(element => <p key={element}>{element}</p>)}
+            {jsonData.statistics.map(element => <p key={element}>{element}</p>)}
         </>
     )
     ingr = (
         <>
-            {jsonData.Ingredients.map((element, index) => <ListElement label={element} id={"ingr" + index} key={element} />)}
+            {jsonData.ingredients.map((element, index) => <ListElement label={element} id={"ingr" + index} key={element} />)}
         </>
     )
     dirs = (
         <>
-            {jsonData.Directions.map((element, index) => <ListElement label={element} id={"dirs" + index} key={element} />)}
+            {jsonData.directions.map((element, index) => <ListElement label={element} id={"dirs" + index} key={element} />)}
         </>
     )
     return [title, stats, ingr, dirs]
--- pages/[recipe_id].js
+++ pages/[recipe_id].js
@@ -2,53 +2,77 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { Inter } from 'next/font/google'
 import { useRouter } from 'next/router'
+import { useEffect, useState } from "react"
+import { ref as ref_db, get } from 'firebase/database'
+import { ref as ref_store, getBlob } from 'firebase/storage'
+import { database, storage } from "@/firebase"
 import styles from '@/styles/Home.module.css'
 import Page from '@/components/Page'
 import ParsedContent from '@/components/ParsedContent.jsx'
-import dataFile from '@/public/recipes.json'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function RecipeWrapper() {
   const router = useRouter()
   const { recipe_id } = router.query
+  const [hasContent, setHasContent] = useState(false)
+  const [title, setTitle] = useState("")
+  const [stats, setStats] = useState("")
+  const [ingr, setIngr] = useState("")
+  const [dirs, setDirs] = useState("")
+  const [img, setImg] = useState("")
 
-  if (!(recipe_id in dataFile)) {
-    return (<div className={styles.no_recipe}>404: Recipe Not Found! ☹️</div>)
-  }
+  useEffect(() => {
+    get(ref_db(database, `recipes/${recipe_id}`)).then((snapshot) => {
+      if (snapshot.exists()) {
+        const [t, s, i, d] = ParsedContent(snapshot.val())
+        setTitle(t)
+        setStats(s)
+        setIngr(i)
+        setDirs(d)
+        setHasContent(true)
+      }
+    }).catch((e) => {
+      console.log(e)
+      setHasContent(false)
+    })
 
-  let jsonData = JSON.parse(JSON.stringify(dataFile[recipe_id]))
-  const [title, stats, ingr, dirs] = ParsedContent(jsonData);
-  const img = `/recipe_images/${recipe_id}.jpeg`;
+    getBlob(ref_store(storage, `images/${recipe_id}.jpeg`)).then(
+      (file) => setImg(URL.createObjectURL(file))
+    ).catch((error) => console.error(error))
+  }, [])
 
   let content = (
-    <>
-      <div className={styles.thumbnail}>
-        <Image className={styles.img_thumbnail} src={img} alt={title} width={300} height={220} />
-      </div>
-      <div className={styles.title}>
-        <h1>{title}</h1>
-      </div>
-      <div className={styles.recipe_wrapper}>
-        <div className={styles.left}>
-          <h2>Statistics</h2>
-          {stats}
+    (!hasContent ?
+      <div className={styles.no_recipe}>404: Recipe Not Found! ☹️</div>
+      : <>
+        {img && <div className={styles.thumbnail}>
+          <Image className={styles.img_thumbnail} src={img} alt={title} width={300} height={220} />
+        </div>}
+        <div className={styles.title}>
+          <h1>{title}</h1>
         </div>
-        <div className={styles.ingr}>
-          <h2>Ingredients</h2>
-          {ingr}
+        <div className={styles.recipe_wrapper}>
+          <div className={styles.left}>
+            <h2>Statistics</h2>
+            {stats}
+          </div>
+          <div className={styles.ingr}>
+            <h2>Ingredients</h2>
+            {ingr}
+          </div>
+          <div className={styles.dirs}>
+            <h2>Directions</h2>
+            {dirs}
+          </div>
         </div>
-        <div className={styles.dirs}>
-          <h2>Directions</h2>
-          {dirs}
+        <div className={styles.center}>
+          <Link href="/" className={styles.back}>
+            <p className={inter.className}><span>&lt;-</span> Back to Home</p>
+          </Link>
         </div>
-      </div>
-      <div className={styles.center}>
-        <Link href="/" className={styles.back}>
-          <p className={inter.className}><span>&lt;-</span> Back to Home</p>
-        </Link>
-      </div>
-    </>
+      </>
+    )
   )
 
   return <Page title={title} content={content} state="Recipe" />
